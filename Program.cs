44c82ba591737fa8b43ddd6b231bce23f14e3252
LHT082024 <LHT082024@gmail.com>
2024-09-26 13:42:52 +0200

    if statment

--- Program.cs
+++ Program.cs
@@ -9,6 +9,20 @@ class Program
     static void Main(string[] args)
     {
         try {
+            //filstien for person.json
+             string filePath = "Person.json";
+
+             List<Person>? people = new List<Person>();
+             if(File.Exists(filePath))
+             {
+                string existingJSON = File.ReadAllText(filePath);
+                Console.WriteLine($"Data already exists in person.json {File.ReadAllText(filePath)}");
+                if(!string.IsNullOrWhiteSpace(existingJSON))
+                {
+                     people = JsonSerializer.Deserialize<List<Person>>(existingJSON);
+                }
+             }
+
             // console write line skriver koden ut mens linjer som "variabel? navn = Console.Readline();"
             //tar og sier at user input er verdien til variabelen. 
             Console.WriteLine("What is your name?");
@@ -36,7 +50,6 @@ class Program
             Console.WriteLine($"Your name is {person.Name}\n you are {person.Age} years old\n and you are from {person.City}");
 
             string json = JsonSerializer.Serialize(person, new JsonSerializerOptions{WriteIndented = true}); 
-            string filePath = "Person.json";
             File.WriteAllText(filePath, json);
 
             Console.WriteLine("Data was written on the JSON object");
